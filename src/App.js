a980a9c039257432825c4d72d481aec68d1c4453
boldsoftmations <vivek@glutape.com>
2024-04-24 12:27:41 +0530

    feat : login form created

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import { useState, useCallback } from "react";
 import "./App.css";
+import whatsappSVG from './Images/whatsappSVG.svg'; 
 
 function App() {
-  const [inputs, setInputs] = useState([]);
+  const [inputs, setInputs] = useState({});
 
-  const handleSubmit = useCallback(() => {
-    console.log("submit function")
-    console.log("submitted", inputs);
-  },[inputs])
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+    console.log("Submitted", inputs);
+  }, [inputs]);
 
   const handleChange = useCallback((event) => {
-    console.log("input changes");
     const { name, value } = event.target;
-    setInputs((prev) => ({ ...prev, [name]: value }));
-  },[])
+    setInputs(prev => ({ ...prev, [name]: value }));
+  }, []);
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <h1>this is form</h1>
-        <label>
-          Enter your name:
-          <input
-            type="text"
-            name="username"
-            value={inputs.username || ""}
-            onChange={(event, value) => handleChange(event)}
-          />
-        </label>
-        <lable>
-          Enter Your Password :
-          <input
-            type="password"
-            name="password"
-            value={inputs.password}
-            onChange={(event, value) => handleChange(event)}
-          />
-        </lable>
-        <input type="submit" />
-      </form>
+    <div className="split left">
+      <img src={whatsappSVG} alt="WhatsApp" />
+    </div>
+    <div className="split right">
+      <h1 className="company-name">BoldSoftmation LLP</h1>
+      <div className="centered">
+          <form onSubmit={handleSubmit} className="login-form">
+            <div className="form-control">
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+                value={inputs.username || ""}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={inputs.password || ""}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit" className="submit-button">Log In</button>
+            <div className="change-password-link">
+            <a href="/change-password">Change Password?</a>
+          </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
